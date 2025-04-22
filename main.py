6fa155c708b84e80a9e919e3daac14f0f3a29035
Kaiyi952 <irishe952@gmail.com>
2025-04-22 13:43:11 -0400

    feat

---
+++ main.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o=mini")
+response = llm.invoke("What is the meanig of life?")
+
+
+
